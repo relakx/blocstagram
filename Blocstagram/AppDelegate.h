da564899a5d838a0d5578b06cee8fc892d7aa3f7
Joseph Blanco <joefblanco@gmail.com>
2015-06-11 19:10:19 -0400

    Initial Commit

---
+++ Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Joseph Blanco on 6/11/15.
+//  Copyright (c) 2015 Blancode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

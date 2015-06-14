dfbc9031c32b8b99632cc7815177a1cd117e3ad4
Joseph Blanco <joefblanco@gmail.com>
2015-06-14 15:35:45 -0400

    Table View controller implemented

---
+++ Blocstagram/BLCImagesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLCImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Joseph Blanco on 6/11/15.
+//  Copyright (c) 2015 Blancode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCImagesTableViewController : UITableViewController
+
+@end
